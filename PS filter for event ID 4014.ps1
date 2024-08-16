4abfb925c4d106b1ca841b93d23d51b2e898476f
John Clark <jclark400@outlook.com>
2024-08-15 18:20:28 -0700

    Working script

--- PS filter for event ID 4014.ps1
+++ PS filter for event ID 4014.ps1
@@ -1,8 +1,9 @@
 # Define the filter for event ID 4014
 $filter = @{
     LogName = 'Microsoft-Windows-PowerShell/Operational'
-    Id = 4014
+    Id = 4104
 }
 
 # Get the events
-Get-WinEvent -FilterHashtable $filter
+Get-WinEvent -FilterHashtable $filter -MaxEvents 5
+
